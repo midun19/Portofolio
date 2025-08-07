35660749fcb7712633aabf8f37e314660d39e9f2
midun19 <zhillanh19@gmail.com>
2025-08-07 23:01:14 +0700

    update : perubahan toaster

--- src/components/ui/toaster.jsx
+++ src/components/ui/toaster.jsx
@@ -8,7 +8,7 @@ import {
   ToastViewport,
 } from "@/components/ui/toast";
 
-export function Toaster() {
+const Toaster = () => {
   const { toasts } = useToast();
 
   return (
@@ -30,4 +30,6 @@ export function Toaster() {
       <ToastViewport />
     </ToastProvider>
   );
-}
+};
+
+export default Toaster;
